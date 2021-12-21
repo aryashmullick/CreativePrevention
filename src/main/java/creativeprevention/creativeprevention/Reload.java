381a11a1bc10228678d8c62aff8124ac6ce3b9d0
AR 9157 <85992871+AR9157@users.noreply.github.com>
2021-12-21 21:51:56 +0530

    Code

---
+++ src/main/java/creativeprevention/creativeprevention/Reload.java
@@ -0,0 +1,44 @@
+package creativeprevention.creativeprevention;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+
+public class Reload implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(cmd.getName().equalsIgnoreCase("cp")){
+            try {
+                String argOne = String.valueOf(args[0]);
+                if (argOne.equalsIgnoreCase("reload")){
+                    if(!(sender instanceof Player) || sender.isOp()) {
+                        CreativePrevention.getInstance().reloadConfig();
+                        sender.sendMessage(ChatColor.GOLD + "[CreativePrevention] " + ChatColor.GREEN + "Config reloaded!");
+                        return true;
+                    }else{
+                        sender.sendMessage(ChatColor.GOLD + "[CreativePrevention] "+ChatColor.RED+"You do not have the required permissions to run this command!");
+                        return true;
+                    }
+                } else if(argOne.equalsIgnoreCase("help")){
+                    sender.sendMessage(ChatColor.GOLD + "[CreativePrevention] " + ChatColor.DARK_GRAY + "Version: " + ChatColor.BLUE+ CreativePrevention.version);
+                    sender.sendMessage(ChatColor.GOLD + "[CreativePrevention] " + ChatColor.AQUA + "/cp help" + ChatColor.GREEN+ " -> This command");
+                    sender.sendMessage(ChatColor.GOLD + "[CreativePrevention] " + ChatColor.AQUA + "/cp reload" + ChatColor.GREEN+ " -> Reloads configuration file");
+                    return true;
+                } else{
+                    sender.sendMessage(ChatColor.GOLD+"[CreativePrevention] "+ChatColor.RED+"Invalid Command. Use "+ChatColor.BLUE+"/cp help");
+                    return true;
+                }
+            }catch(Exception e){
+                sender.sendMessage(ChatColor.GOLD+"[CreativePrevention] "+ChatColor.RED+"Invalid Command. Use "+ChatColor.BLUE+"/cp help");
+            }
+        }
+
+        return true;
+    }
+}
